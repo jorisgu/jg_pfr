0254cf265620f6f2c55ba20c842684eafcfd0c90
Joris Guerry <joguerry+git@gmail.com>
2017-01-31 17:21:48 +0100

    exctract updated with PLDI

--- tools/jg_extract_set_from_dataset.sh
+++ tools/jg_extract_set_from_dataset.sh
@@ -19,7 +19,7 @@ sets_dir=$1/data/sets/$3/
 for stage in 'trainGupta' 'valGupta' 'trainvalGupta' 'testGupta'
 do
   echo "Processing ${stage} set :"
-  for encoding in 'labels_segmentation_37' 'd_raw_cubehelix_8bits' 'd_raw_histeqBack_8bits' 'd_raw_histeqBack_16bits' 'd_raw_histeqBack_cubehelix_8bits' 'd_raw_histeqBack_jet_8bits' 'd_raw_histeqFront_8bits' 'd_raw_histeqFront_16bits' 'd_raw_histeqFront_cubehelix_8bits' 'd_raw_histeqFront_jet_8bits' 'd_raw_histeqRandom_8bits' 'd_raw_histeqRandom_16bits' 'd_raw_histeqRandom_cubehelix_8bits' 'd_raw_histeqRandom_jet_8bits' 'd_raw_jet_8bits' 'd_raw_normal_8bits' 'd_raw_normal_16bits' 'rgbd_egh_8bits' 'rgb_i_10_8bits' 'rgb_i_20_8bits' 'rgb_i_30_8bits' 'rgb_i_40_8bits' 'rgb_i_50_8bits' 'rgb_i_60_8bits' 'rgb_i_70_8bits' 'rgb_i_80_8bits' 'rgb_i_90_8bits' 'rgb_i_100_8bits'
+  for encoding in 'labels_segmentation_37' 'd_raw_PLDI_16bits' 'd_raw_cubehelix_8bits' 'd_raw_histeqBack_8bits' 'd_raw_histeqBack_16bits' 'd_raw_histeqBack_cubehelix_8bits' 'd_raw_histeqBack_jet_8bits' 'd_raw_histeqFront_8bits' 'd_raw_histeqFront_16bits' 'd_raw_histeqFront_cubehelix_8bits' 'd_raw_histeqFront_jet_8bits' 'd_raw_histeqRandom_8bits' 'd_raw_histeqRandom_16bits' 'd_raw_histeqRandom_cubehelix_8bits' 'd_raw_histeqRandom_jet_8bits' 'd_raw_jet_8bits' 'd_raw_normal_8bits' 'd_raw_normal_16bits' 'rgbd_egh_8bits' 'rgb_i_10_8bits' 'rgb_i_20_8bits' 'rgb_i_30_8bits' 'rgb_i_40_8bits' 'rgb_i_50_8bits' 'rgb_i_60_8bits' 'rgb_i_70_8bits' 'rgb_i_80_8bits' 'rgb_i_90_8bits' 'rgb_i_100_8bits'
     do
   	echo "Encoding : ${encoding}"
       imageset_file=${sets_dir}${stage}.txt
