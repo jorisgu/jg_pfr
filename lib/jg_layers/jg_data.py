c584668797417d2edbe764c9e7854718c7c0cda3
jorisgu <joguerry+git@gmail.com>
2017-02-14 16:46:58 +0100

    jg_data_2 ok sauf accuracyJG

--- lib/jg_layers/jg_data.py
+++ lib/jg_layers/jg_data.py
@@ -25,8 +25,17 @@ class dataset:
     def __call__(self):
         pass
 
+def get_classes(classes_file_path):
+    classes = []
+    with open(classes_file_path, 'r') as fp:
+        for line in fp:
+            if line[0]=='#' or line[0]=='\n':
+                pass
+            else:
+                classes.append(line[:-1])
+    return classes
 
-def prepareViews(img_w, img_h, ratio_for_stride=0.5, min_size_min=64, min_size_max=128, min_size_step=16, verbose=False):
+def prepareViews(img_w, img_h, ratio_for_stride=0.5, min_size_min=128, min_size_max=128, min_size_step=16, verbose=False):
     min_size_max +=1
     if verbose:
         print('img_size : ' + str(img_w) + ',' +str(img_h))
@@ -47,7 +56,7 @@ def prepareViews(img_w, img_h, ratio_for_stride=0.5, min_size_min=64, min_size_m
                 if verbose:
                     print('Find : ' + str(i_w)+','+ str(i_h))
                 # views.append([i_w, i_h, min_size, min_size]) # x y w h
-                views.append([i_w, i_h, i_w + min_size-1, i_h + min_size-1]) #x1 y1 x2 y2
+                views.append([i_w, i_h, i_w + min_size, i_h + min_size]) #x1 y1 x2 y2
                 i_h += stride
             i_w += stride
     if verbose:
@@ -423,6 +432,235 @@ class jg_input_test_layer(caffe.Layer):
         segmentation = segmentation[np.newaxis, ...]
         return segmentation
 
+class jg_input_fuse_segmentation_batch_layer(caffe.Layer):
+
+    def setup(self, bottom, top):
+        """Setup the layer."""
+
+
+        # tops: check configuration
+        if len(top) != 3:
+            raise Exception("Need to define {} tops for all outputs.")
+        # data layers have no bottoms
+        if len(bottom) != 0:
+            raise Exception("Do not define a bottom.")
+
+        layer_params = yaml.load(self.param_str_)
+
+
+
+        self.dummy_data = layer_params['dummy_data']
+        self.data_way = layer_params['data_way']
+        self.batch_size = layer_params['batch_size']
+
+
+        self.images_folder_0 = layer_params['images_folder_0']
+        self.image_file_extension_0 = layer_params['image_file_extension_0']
+        self.images_folder_1 = layer_params['images_folder_1']
+        self.image_file_extension_1 = layer_params['image_file_extension_1']
+
+        self.segmentations_folder = layer_params['segmentations_folder']
+        self.segmentation_file_extension = layer_params['segmentation_file_extension']
+
+        self.set_file_path = layer_params['set_file_path']
+        self.classes_file_path = layer_params['classes_file_path']
+
+        # classes
+        self.classes = []
+        self.classes = get_classes(self.classes_file_path)
+        self.num_classes = len(self.classes)
+        self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
+
+        # images
+        self.list_images = open(self.set_file_path, 'r').read().splitlines()
+        self.nb_images = len(self.list_images)
+        print "Number of image :",self.nb_images
+        print "Separated in", self.num_classes, "classes :", self._class_to_ind
+
+
+
+        # shuffling at each epoch
+        self.shuffle = layer_params['shuffle']
+        self.permutation = []
+        self.countEpoch = 0
+        if self.shuffle:
+            print "Shuffling activated."
+            self.permutation = np.random.permutation(self.nb_images)
+        else:
+            self.permutation = range(self.nb_images)
+
+        #tmp image in layer
+        self.views = []
+        self.image_0 = np.zeros((1,3,128,128), dtype=np.float32)
+        self.image_1 = np.zeros((1,3,128,128), dtype=np.float32)
+        self.segmentation = np.zeros((1,3,128,128), dtype=np.float32)
+
+        # init batch data
+        self.batch_data_0 = np.zeros((int(self.batch_size),3,128,128), dtype=np.float32)
+        self.batch_data_1 = np.zeros((int(self.batch_size),3,128,128), dtype=np.float32)
+        self.batch_segmentation = np.zeros((int(self.batch_size),1,128,128), dtype=np.uint8)
+
+        # counters
+        self.views_processed = 0
+        self.images_processed = 0
+        self.patches_processed = 0
+        self.batches_processed = 0
+        self.nb_epochs = 0  # setup a counter for iteration
+        self.needToChangeImage = True
+
+    def update(self,newParams):
+        for key in newParams.keys():
+            setattr(self, key, newParams[key])
+
+        self.classes = get_classes(self.classes_file_path)
+        self.num_classes = len(self.classes)
+        self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
+
+        # images
+        self.list_images = open(self.set_file_path, 'r').read().splitlines()
+        self.nb_images = len(self.list_images)
+        #print "Number of image :",self.nb_images
+        #print "Separated in", self.num_classes, "classes :", self._class_to_ind
+
+    def forward(self, bottom, top):
+        """
+        Top order :
+            #data
+            #data_
+            #segmentation
+        """
+        # fill blob with data
+        top[0].data[...] = self.batch_data_0
+        top[1].data[...] = self.batch_data_1
+        top[2].data[...] = self.batch_segmentation
+        # Update number of batches processed
+        self.batches_processed += 1
+
+    def backward(self, top, propagate_down, bottom):
+        pass
+
+    def reshape(self, bottom, top):
+        if self.data_way=='views':
+            self.load_data_view()
+        elif self.data_way=='full':
+            self.load_data_full()
+        # reshape net
+        top[0].reshape(*self.batch_data_0.shape)
+        top[1].reshape(*self.batch_data_1.shape)
+        top[2].reshape(*self.batch_segmentation.shape)
+
+    def load_data_view(self):
+        """
+        Load input image and preprocess for Caffe:
+        - extract only a sub crop of the image
+        - cast to float
+        - switch channels RGB -> BGR
+        - subtract mean
+        - transpose to channel x height x width order
+        """
+
+        for i in range(int(self.batch_size)):
+            if self.patches_processed>=len(self.views):
+                self.needToChangeImage = True
+            if self.needToChangeImage:
+                self.needToChangeImage = False
+                self.views_processed=0
+                self.images_processed+=1
+                if self.images_processed>=self.nb_images:
+                    self.countEpoch += 1
+                    if self.shuffle:
+                        self.permutation = np.random.permutation(self.nb_images)
+
+                img_0 = Image.open('{}{}.{}'.format(self.images_folder_0, self.list_images[self.permutation[self.images_processed%self.nb_images]],self.image_file_extension_0))
+                img_0 = np.array(img_0, dtype=np.float32)
+                if len(img_0.shape)==3:
+                    img_0 = img_0[:,:,::-1]
+                    #img -= self.mean_bgr
+                    img_0 = img_0.transpose((2,0,1))
+                elif len(img_0.shape)==2:
+                    img_0 = img_0[np.newaxis, ...]
+                else:
+                    print "Error : shape not accepted for img_0."
+
+                img_1 = Image.open('{}{}.{}'.format(self.images_folder_1, self.list_images[self.permutation[self.images_processed%self.nb_images]],self.image_file_extension_1))
+                img_1 = np.array(img_1  , dtype=np.float32)
+                if len(img_1.shape)==3:
+                    img_1 = img_1[:,:,::-1]
+                    #img -= self.mean_bgr
+                    img_1 = img_1.transpose((2,0,1))
+                elif len(img_1.shape)==2:
+                    img_1 = img_1[np.newaxis, ...]
+                else:
+                    print "Error : shape not accepted for img_1."
+
+                seg = Image.open('{}{}.{}'.format(self.segmentations_folder, self.list_images[self.permutation[self.images_processed%self.nb_images]],self.segmentation_file_extension))
+                seg = np.array(seg, dtype=np.uint8)
+                seg = seg[np.newaxis, ...]
+
+                # recompute views coordinates if the shape of image
+                if self.image_0.shape!=img_0.shape or len(self.views)==0:
+                    self.views = prepareViews(img_0.shape[2], img_0.shape[1], ratio_for_stride=0.5, min_size_min=128, min_size_max=128, min_size_step=64, verbose=False)
+
+                self.image_0 = img_0
+                self.image_1 = img_1
+                self.segmentation = seg
+
+            img_patch_0 = self.image_0[:,self.views[self.views_processed][1]:self.views[self.views_processed][3],self.views[self.views_processed][0]:self.views[self.views_processed][2]]
+            img_patch_1 = self.image_1[:,self.views[self.views_processed][1]:self.views[self.views_processed][3],self.views[self.views_processed][0]:self.views[self.views_processed][2]]
+            seg_patch = self.segmentation[:,self.views[self.views_processed][1]:self.views[self.views_processed][3],self.views[self.views_processed][0]:self.views[self.views_processed][2]]
+
+            self.batch_data_0[i,...] = img_patch_0
+            self.batch_data_1[i,...] = img_patch_1
+            self.batch_segmentation[i,...] = seg_patch
+
+            self.views_processed+=1
+            self.patches_processed+=1
+
+    def load_data_full(self):
+        """
+        Load input image and preprocess for Caffe:
+        - cast to float
+        - switch channels RGB -> BGR
+        - subtract mean
+        - transpose to channel x height x width order
+        """
+        img_0 = Image.open('{}{}.{}'.format(self.images_folder_0, self.list_images[self.permutation[self.images_processed]],self.image_file_extension_0))
+        img_0 = np.array(img_0, dtype=np.float32)
+        if len(img_0.shape)==3:
+            img_0 = img_0[:,:,::-1]
+            #img_0 -= self.mean_bgr
+            img_0 = img_0.transpose((2,0,1))
+        elif len(img_0.shape)==2:
+            img_0 = img_0[np.newaxis, ...]
+        else:
+            print "Error : shape not accepted for img."
+
+
+        img_1 = Image.open('{}{}.{}'.format(self.images_folder_1, self.list_images[self.permutation[self.images_processed]],self.image_file_extension_1))
+        img_1 = np.array(img_1, dtype=np.float32)
+        if len(img_1.shape)==3:
+            img_1 = img_1[:,:,::-1]
+            #img_1 -= self.mean_bgr
+            img_1 = img_1.transpose((2,0,1))
+        elif len(img_1.shape)==2:
+            img_1 = img_1[np.newaxis, ...]
+        else:
+            print "Error : shape not accepted for img_."
+
+        seg = Image.open('{}{}.{}'.format(self.segmentations_folder, self.list_images[self.permutation[self.images_processed]],self.segmentation_file_extension))
+        seg = np.array(seg, dtype=np.uint8)
+        seg = seg[np.newaxis, ...]
+
+        self.batch_data_0 = img_0[np.newaxis, ...]
+        self.batch_data_1 = img_1[np.newaxis, ...]
+        self.batch_segmentation = seg[np.newaxis, ...]
+
+        self.images_processed+=1
+        if self.images_processed>=self.nb_images:
+            self.countEpoch += 1
+            if self.shuffle:
+                self.permutation = np.random.permutation(self.nb_images)
+
 class jg_input_fuse_segmentation_layer(caffe.Layer):
     """works with only on image for now by batch"""
 
@@ -675,7 +913,7 @@ class jg_input_fuse_segmentation_layer(caffe.Layer):
         else:
             print "Error : shape not accepted."
         if self.dummy_data:
-            return np.ones((3,64,64),dtype=np.float32)
+            return np.ones((3,128,128),dtype=np.float32)
         else:
             return in_
         self.batch_data = self.load_img_data(self.iter_counter)[np.newaxis, ...]
@@ -694,7 +932,7 @@ class jg_input_fuse_segmentation_layer(caffe.Layer):
         segmentation = segmentation[np.newaxis, ...]
 
         if self.dummy_data:
-            return np.ones((1,64,64),dtype=np.float32)
+            return np.ones((1,128,128),dtype=np.float32)
         else:
             return segmentation
 
@@ -772,7 +1010,6 @@ class jg_input_segmentation_layer(caffe.Layer):
         self.nb_images = len(self.list_images)
         #print "Number of image :",self.nb_images
         #print "Separated in", self.num_classes, "classes :", self._class_to_ind
-
 
     def forward(self, bottom, top):
         """
@@ -905,7 +1142,7 @@ class jg_rebatching_layer(caffe.Layer):
         # get size of feature blob
         # newbatch has same deepness
 
-        newbatch = np.zeros([256, 3, 480, 640])
+        newbatch = np.zeros([256, 3, 480, 1280])
 
         im = Image.open('{}{}.{}'.format(self.images_folder, self.list_images[idx],self.image_file_extension))
         in_ = np.array(im, dtype=np.float32)
